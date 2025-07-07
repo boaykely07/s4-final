801c5d867334cbcb1e63b9db7c99e2ff705f06c4
AndoraRavelo <andoniainarandrianarivelo28@gmail.com>
2025-07-07 17:38:00 +0300

    Insertion_fonds_Ok

--- ws/routes/routes.php
+++ ws/routes/routes.php
@@ -1,4 +1,7 @@
 <?php
 require_once __DIR__ . '/../controllers/EtudiantController.php';
+require_once __DIR__ . '/../controllers/FondsController.php';
 
 Flight::route('GET /etudiants', ['EtudiantController', 'getAll']);
+Flight::route('POST /fonds', ['FondsController', 'create']);
+
